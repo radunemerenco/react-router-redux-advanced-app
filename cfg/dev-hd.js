c36b0b3440d8843f00207998ec9fc5d82e80fe9b
Radu Nemerenco <rnemerenco@styleze.com>
2017-04-07 15:50:49 +0300

    Optimize configurations

--- cfg/dev-hd.js
+++ cfg/dev-hd.js
@@ -1,38 +1,10 @@
 'use strict';
 
-let path = require('path');
-let webpack = require('webpack');
-
-let baseConfig = require('./base');
-let defaultSettings = require('./defaults');
-
-let config = Object.assign({}, baseConfig, {
-  entry: [
-    'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
-  cache: true,
-  devtool: 'eval-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
-  module: defaultSettings.getDefaultModules()
-});
-// Add needed loaders to the defaults here
-config.module.loaders.push({
-  test: /\.(js|jsx)$/,
-  loader: 'react-hot-loader!babel-loader',
-  include: [].concat(
-    defaultSettings.additionalPaths,
-    [ path.join(__dirname, '/../src') ]
-  )
-});
+let devConfig = require('./dev');
+let config = Object.assign({}, devConfig);
 
 config.resolve.alias = Object.assign({}, config.resolve.alias, {
   mainPlugin: `./plugins/homedepot`,
 });
-
 
 module.exports = config;
--- cfg/dist-hd.js
+++ cfg/dist-hd.js
@@ -1,35 +1,7 @@
 'use strict';
 
-let path = require('path');
-let webpack = require('webpack');
-
-let baseConfig = require('./base');
-let defaultSettings = require('./defaults');
-
-let config = Object.assign({}, baseConfig, {
-  entry: path.join(__dirname, '../src/index'),
-  cache: false,
-  devtool: 'sourcemap',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
-  module: defaultSettings.getDefaultModules()
-});
-
-// Add needed loaders to the defaults here
-config.module.loaders.push({
-  test: /\.(js|jsx)$/,
-  loader: 'babel-loader',
-  include: [].concat(
-    defaultSettings.additionalPaths,
-    [ path.join(__dirname, '/../src') ]
-  )
-});
+let distConfig = require('./dist');
+let config = Object.assign({}, distConfig);
 
 config.resolve.alias = Object.assign({}, config.resolve.alias, {
   mainPlugin: `./plugins/homedepot`,
